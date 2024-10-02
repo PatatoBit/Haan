7c349131e9ea5fabf96adede57f2bfafe0335902
Pat <patato@Depas-MacBook-Air-81.local>
2024-10-02 10:10:43 +0700

    Init

---
+++ Haan/HaanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaanApp.swift
+//  Haan
+//
+//  Created by Pat on 2/10/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct HaanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HaanTests/HaanTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaanTests.swift
+//  HaanTests
+//
+//  Created by Pat on 2/10/2567 BE.
+//
+
+import Testing
+@testable import Haan
+
+struct HaanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
